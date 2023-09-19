fbdea6e1f9a3ec68c5b1cbd768edf233d8f94942
Eric <menaericdaniel@gmail.com>
2023-09-19 09:52:28 -0300

    agrega funcionalidad multiplicar

--- app.js
+++ app.js
@@ -1,26 +1,23 @@
 //todo: hacer el front de la calculadora
-const process = require('process');
-const restar = require('./restar');
-const {argv} = process;
-const sumar = require('./sumar')
+const process = require("process");
+const restar = require("./restar");
+const { argv } = process;
+const sumar = require("./sumar");
+const multiplicar = require("./multiplicar");
 
-function caluladora(comando, a,b) {
-    switch (comando) {
-        case "sumar":
-            return sumar(a,b)
-            break;
-            case "restar":
-            return restar(a,b)
-            case "multiplicar":
-            
-            break;
-            case "dividir":
-            
-            break;
-        default:
-            return "comando no válido"
-    }
+function caluladora(comando, a, b) {
+  switch (comando) {
+    case "sumar":
+      return sumar(a, b);
+    case "restar":
+      return restar(a, b);
+    case "multiplicar":
+      return multiplicar(a,b);
+    case "dividir":
+      break;
+    default:
+      return "comando no válido";
+  }
 }
 
-console.log(caluladora(argv[2],+argv[3],+argv[4]));
-
+console.log(caluladora(argv[2], +argv[3], +argv[4]));
